3dbb90cc41f131d327cc711b2fa46c658e9cd0ed
Lsmaxs <shuan934321631@qq.com>
2017-10-17 20:17:57 +0800

    about 完成

--- personal/containers/about/index.jsx
+++ personal/containers/about/index.jsx
@@ -4,8 +4,8 @@ import FooterComponnet from '../../components/footer/index.jsx';
 import HeaderComponent from '../../components/header/index.jsx';
 import Banner from './banner.png';
 import Me from './touxiang.jpg';
-import LiveOne from './touxiang.jpg';
-import LiveTwo from './touxiang.jpg';
+import LiveOne from './toew.jpg';
+import LiveTwo from './noe.jpg';
 class About extends Component {
     constructor(){
         super();
