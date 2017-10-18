07dc58c88eb8db76e3cc6d5f49f3da8dd19bb14b
sixGod <linshuan@uartbaby.com>
2017-10-18 12:01:07 +0800

    test

--- README.md
+++
@@ -1,6 +0,0 @@
-# Lsmax.github.io
-my web sitb
-
-> 个人网站 初始化
-
->持续开发中
--- personal/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './root/index.jsx';
-import './static/scss/reset.scss';
-ReactDOM.render(
-    <Root/>,
-    document.getElementById("app")
-);
